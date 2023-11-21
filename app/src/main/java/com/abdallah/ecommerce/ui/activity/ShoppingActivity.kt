d1ed2c3aafbb99c760e33f9df2e13fe85bfb462b
Abdalla Marwad <abdallahshehata311as@gmail.com>
2023-11-21 09:40:45 +0200

    feat : add nav graph for shopping

--- app/src/main/java/com/abdallah/ecommerce/ui/activity/ShoppingActivity.kt
+++ app/src/main/java/com/abdallah/ecommerce/ui/activity/ShoppingActivity.kt
@@ -2,11 +2,22 @@ package com.abdallah.ecommerce.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.abdallah.ecommerce.R
+import com.abdallah.ecommerce.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityShoppingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        binding = ActivityShoppingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpNavController()
+    }
+    private fun setUpNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
     }
 }
